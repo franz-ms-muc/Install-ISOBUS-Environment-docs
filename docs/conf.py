7a9104902d01e79fd5178011f79794c7b053071b
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2022-08-28 10:04:38 +0200

    mistake !

--- docs/conf.py
+++ docs/conf.py
@@ -2,17 +2,17 @@
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
-# <https://www.sphinx-doc.org/en/master/usage/configuration.html
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc>`_ are in another directory,
+# If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 # import os
 # import sys
-# sys.path.insert(0, os.path.abspath('.'>`_>`_
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -25,7 +25,7 @@ author = "Read the Docs core team"
 # -- General configuration ---------------------------------------------------
 # -- General configuration
 
-extensions = `
+extensions = [
     "sphinx.ext.duration",
     "sphinx.ext.doctest",
     "sphinx.ext.autodoc",
@@ -34,13 +34,13 @@ extensions = `
 ]
 
 intersphinx_mapping = {
-    "rtd": ("<https://docs.readthedocs.io/en/stable/", None>`_,
-    "python": ("<https://docs.python.org/3/", None>`_,
-    "sphinx": ("<https://www.sphinx-doc.org/en/master/", None>`_,
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
-intersphinx_disabled_domains = `"std"]
+intersphinx_disabled_domains = ["std"]
 
-templates_path = `"_templates"]
+templates_path = ["_templates"]
 
 # -- Options for EPUB output
 epub_show_urls = "footnote"
@@ -48,7 +48,7 @@ epub_show_urls = "footnote"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = `"_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -57,7 +57,7 @@ exclude_patterns = `"_build", "Thumbs.db", ".DS_Store"]
 #
 html_theme = "sphinx_rtd_theme"
 
-# Add any paths that contain custom static files (such as style sheets>`_ here,
+# Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = `"_static"]
+html_static_path = ["_static"]
